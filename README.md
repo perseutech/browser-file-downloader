021d639330ef33223d48a14bf91a45594ffb28c4
Alison Moura <alison.oghino@gmail.com>
2020-06-16 01:46:03 -0400

    Rename package and setup production build

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# File Downloader
+# Browser File Downloader
 
-A simple JavaScript project for download file from a Blob.
+A simple JavaScript module for download file from a Blob in the Browser-side.
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,49 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
-	mode: 'development',
-	entry: './src/index.ts',
+  mode: process.env.MODE || 'development',
+  entry: './src/index.ts',
 
-	output: {
-		filename: 'index.js',
-		path: path.resolve(__dirname, 'dist')
-	},
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 
-	plugins: [new webpack.ProgressPlugin()],
+  plugins: [new webpack.ProgressPlugin()],
 
-	module: {
-		rules: [
-			{
-				test: /.(ts|tsx)?$/,
-				loader: 'ts-loader',
-				include: [path.resolve(__dirname, 'src')],
-				exclude: [/node_modules/]
-			}
-		]
-	},
+  module: {
+    rules: [
+      {
+        test: /.(ts|tsx)?$/,
+        loader: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')],
+        exclude: [/node_modules/],
+      },
+    ],
+  },
 
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					priority: -10,
-					test: /[\\/]node_modules[\\/]/
-				}
-			},
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/,
+        },
+      },
 
-			chunks: 'async',
-			minChunks: 1,
-			minSize: 30000,
-			name: true
-		}
-	},
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: true,
+    },
+  },
 
-	devServer: {
-		open: true
-	},
+  devServer: {
+    open: true,
+  },
 
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js']
-	}
-};
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+}
